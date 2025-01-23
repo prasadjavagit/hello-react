24d4d5290fa28c181417cbf13581387d6dba1b3c
Prasad <balujustin@gmail.com>
2025-01-23 21:14:29 +0530

    Prettier, ESLint, React Snippet

--- src/BookList.js
+++ src/BookList.js
@@ -1,16 +1,16 @@
 import { books } from './books';
 import Book from './Book';
 const BookList = () => {
-    //view return in jsx
-    return (
-        <>
-            <div class="container text-center mt-3">
-                <h1>Authors</h1>
-            </div>
-            {books.map((book, index) => {
-                return <Book key={index} {...book}></Book>;
-            })}
-        </>
-    );
+  //view return in jsx
+  return (
+    <>
+      <div class="container text-center mt-3">
+        <h1>Authors</h1>
+      </div>
+      {books.map((book, index) => {
+        return <Book key={index} {...book}></Book>;
+      })}
+    </>
+  );
 };
 export default BookList;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import BookList from './BookList'
+import BookList from './BookList';
 
 //we are mounting our BookList component defined in BookList.js to root element
 const root = ReactDOM.createRoot(document.getElementById('root'));
